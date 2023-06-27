7a4213ef2ceca0cd8e15c21c99480c1c694cfe6c
shi <815599298@qq.com>
2023-06-27 17:48:23 +0800

    拐弯处敌人转身
    添加敌人通过拐角路径点时转身

--- Assets/Scripts/EnemyMoveB.cs
+++ Assets/Scripts/EnemyMoveB.cs
@@ -7,6 +7,7 @@ public class EnemyMoveB : MonoBehaviour
     Transform target;
     private float speed = 20f;
     private int pointIndex = 0;
+    private float rotateSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,12 @@ public class EnemyMoveB : MonoBehaviour
     void MoveB(){
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        //敌人通过路径点时旋转
+        Quaternion targetRotation = Quaternion.LookRotation(dir);
+        // 插值旋转，使物体平滑转向目标方向
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+        
         if(Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
             pointIndex++;

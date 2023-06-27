77b6e3858c2747efba312fdc93b92900d3fb706b
shi <815599298@qq.com>
2023-06-27 10:40:37 +0800

    敌人孵化
    按波次生成敌人

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,6 +6,34 @@ public class EnemySpawner : MonoBehaviour
 {
 
     public Wave[] waves;
+    public float waveRate = 5f;
+    public static int CountEnemyAlive = 0;
 
-    
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        foreach (Wave wave in waves)
+        {
+            foreach (EnemyType enemyType in wave.enemyTypes)
+            {
+                for (int i = 0; i < enemyType.count; i++)
+                {
+                    GameObject.Instantiate(enemyType.enemyPrefab, enemyType.startPoint.position, Quaternion.identity);
+                    CountEnemyAlive++;
+                    yield return new WaitForSeconds(1f / enemyType.rate);
+                }
+                while (CountEnemyAlive > 0)
+                {
+                    yield return 0;
+                }
+                yield return new WaitForSeconds(waveRate);
+            }
+        }
+    }
+
 }
+
